d4cf89b38ec165e0e56b5dc1c639ffe96981320e
Lukasz Sielski <lsielski@miovision.com>
2024-09-19 12:33:13 +0200

    test

--- scripts/az_cli.py
+++ scripts/az_cli.py
@@ -1,18 +1,15 @@
-import subprocess
-from subprocess import check_output, CalledProcessError
+from subprocess import check_output, CalledProcessError, STDOUT
 
 class AzCli:
     
-    def tag_work_items(self, work_items_ids: list[str], tag: str):
+    def tag_work_items(self, work_items_ids: list[str], tag: str) -> None:
         update_result = {}
         for work_item_id in work_items_ids:
-            print(f'Trying to tag {work_item_id}')
-            output = None
+            print(f'Trying to tag {work_item_id} with tag {tag}')
             try:
-                output = check_output(f'az boards work-item update --id {work_item_id} --org https://dev.azure.com/lukaszadamsielski0187 --fields "System.Tags={tag}"', shell=True, stderr=subprocess.STDOUT)
-                update_result.update({work_item_id: output})
+                check_output(f'az boards work-item update --id {work_item_id} --org https://dev.azure.com/lukaszadamsielski0187 --fields "System.Tags={tag}"', shell=True, stderr=STDOUT)
+                update_result.update({work_item_id: 'Success'})
             except CalledProcessError as e:
                 print(f"Update process failed for {work_item_id}, error: {e}")
-                print(f"{e.output}/{e.returncode}/{e.stdout}/{e.stderr}")
                 update_result.update({work_item_id: e.output})
         print(update_result)
