e1b376320e87de470b36c3ca90ed340e0519679d
Lukasz Sielski <lsielski@miovision.com>
2024-09-19 12:48:38 +0200

    test

--- scripts/az_cli.py
+++ scripts/az_cli.py
@@ -1,10 +1,10 @@
 from subprocess import check_output, CalledProcessError, STDOUT
 
-def tag_work_items(work_items_ids: list[str], tag: str) -> None:
+def tag_work_items(self, work_items_ids: list[str], tag: str) -> None:
     for work_item_id in work_items_ids:
         print(f'Trying to tag {work_item_id} with tag {tag}')
         try:
-            check_output(f'az boards work-item update --id {work_item_id} --org https://dev.azure.com/lukaszadamsielski0187 --fields "System.Tags={tag}"', shell=True)
+            check_output(f'az boards work-item update --id {work_item_id} --org https://dev.azure.com/lukaszadamsielski0187 --fields "System.Tags={tag}"', shell=True, stderr=STDOUT)
             print(f'Successfully updated work item {work_item_id} with tag {tag}')
         except CalledProcessError as e:
-            print(f"Update process failed for {work_item_id}, error: {e.stderr}")
+            print(f"Update process failed for {work_item_id}, error: {e.output}")
