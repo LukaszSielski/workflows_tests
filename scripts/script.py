8981ed5fda183da82e107c0cf43fe9802a83d63f
Lukasz Sielski <lsielski@miovision.com>
2024-09-19 12:43:24 +0200

    test

--- scripts/script.py
+++ scripts/script.py
@@ -1,13 +1,6 @@
 import argparse
-import subprocess
-from gh_cli import GitHubCli, PrState
-from az_cli import AzCli
-
-class _Const(object):
-    def ADO_TAGGING_COMMAND() -> str:
-        return 'az boards work-item update --id {work_item_id} --org https://dev.azure.com/lukaszadamsielski0187 --fields "System.Tags={deploy_env}" --output json'
-    
-CONST = _Const()
+from gh_cli import extract_data_by_pattern_from_pr_body, PrState
+from az_cli import tag_work_items
 
 def parseArguments():
     parser = argparse.ArgumentParser(description="Utility to tag ADO workitem with deployment environemnt", exit_on_error=False)
@@ -15,23 +8,10 @@ def parseArguments():
     parser.add_argument('--deploy_env', type=str, required=True, dest='deploy_env')
     return parser.parse_args()
 
-def tagAdoWorkItem(workItemId: str, deploymentEnv: str):
-    adoTaggingCommand = CONST.ADO_TAGGING_COMMAND.format(work_item_id = workItemId, deploy_env = deploymentEnv)
-    
-    adoTaggingSubprocessResponse = subprocess.run(adoTaggingCommand, capture_output=True, shell=True, text=True)
-    if adoTaggingSubprocessResponse.returncode != 0:
-        raise Exception('Failed to tag ADO work item identified by id [{}] with deployment env [{}]! Reason: {}'.format(workItemId, deploymentEnv, adoTaggingSubprocessResponse.stderr))
-    print('Successfully tagged work item identified by id [{}] with deployment env [{}].'.format(workItemId, deploymentEnv))
-    
 def main():
     args = parseArguments()
-    gh_cli = GitHubCli()
-    az_cli = AzCli()
-    commitSha = args.commit_sha
-    deploymentEnv = args.deploy_env
-    # tagAdoWorkItem(extractWorkItemIdFromPR(commitSha), deploymentEnv)
-    workItemsIds = gh_cli.extract_data_by_pattern_from_pr_body(commitSha, PrState.MERGED, r'AB#(\d+)')
-    az_cli.tag_work_items(workItemsIds, deploymentEnv)
+    workItemsIds = extract_data_by_pattern_from_pr_body(args.commit_sha, PrState.MERGED, r'AB#(\d+)')
+    tag_work_items(workItemsIds, args.deploy_env)
     
 if __name__ == '__main__':
     try:
