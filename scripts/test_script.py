50c89d4aaafddd9c60a04faf3c02b980baf0604c
Lukasz Sielski <lsielski@miovision.com>
2024-09-24 08:40:32 +0200

    added tests

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -1,8 +1,9 @@
 import unittest
 from unittest import mock
 import argparse
-
+from gh_cli import PrNotFoundException, GhCliException
 import script
+import subprocess
 
 @mock.patch('script.argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(commit_sha='123', deploy_env='dev'))
 @mock.patch('gh_cli.check_output')
@@ -27,3 +28,17 @@ class TestTagging(unittest.TestCase):
             mock.call('az boards work-item update --id 123456 --org https://dev.azure.com/lukaszadamsielski0187 --fields "System.Tags=VNXT CL Dev"', shell=True, stderr=-2),
             mock.call('az boards work-item update --id 654321 --org https://dev.azure.com/lukaszadamsielski0187 --fields "System.Tags=VNXT CL Dev"', shell=True, stderr=-2),
         ])
+    
+    def test_should_throw_exception_if_pr__not_found(self, az_cli_check_output, gh_cli_check_output, parse_args_mock):
+        gh_cli_check_output.return_value = '[]'
+        
+        with self.assertRaises(PrNotFoundException) as e:
+            script.main()
+        self.assertEquals('Could not find pull request for commit identitifed by 123 SHA', str(e.exception))
+        
+    def test_should_throw_exception_if_gh_cli_command_failed(self, az_cli_check_output, gh_cli_check_output, parse_args_mock):
+        gh_cli_check_output.side_effect = subprocess.CalledProcessError('test', 'test', 'Error!')
+        
+        with self.assertRaises(GhCliException) as e:
+            script.main()
+        self.assertEquals('Failed to execute GH CLI command! Error: Error!', str(e.exception))
